11492c67786c3c5e32cd592ca5f9eb42fe3cbbb9
wolfrik17 <cristian.aioanei@gmail.com>
2020-12-02 14:13:58 +0200

    Update Day1.cpp

--- Solutions/Day1.cpp
+++ Solutions/Day1.cpp
@@ -6,7 +6,6 @@ int main()
 {
     std::string number;
     std::vector<int> input;
-    bool found;
     
     while (std::getline(std::cin, number))
     {
@@ -14,28 +13,30 @@ int main()
     }
    
     //Part 1
-    found = 0;
     std::cout << std::endl << "Part 1 answer: ";
     
-    for (auto i = input.begin(); i < input.end()-1 && found == 0; ++i)
-        for (auto y = i + 1; y < input.end() && found == 0; ++y)
+    for (auto i = input.begin(); i < input.end()-1; ++i)
+        for (auto y = i + 1; y < input.end(); ++y)
             if (*i + *y == 2020)
             {
                 std::cout << *i << " x " << *y << " = " << (*i) * (*y) << std::endl;
-                found = 1;
+                goto part2;
             }
-    
     //Part 2
-    found = 0;
+    part2:
     std::cout << std::endl << "Part 2 answer: ";
     
-    for (auto i = input.begin(); i < input.end()-2 && found == 0; ++i)
-        for (auto y = i + 1; y < input.end()-1 && found == 0; ++y)
-            for (auto z = y + 1; z < input.end() && found == 0; ++z)
-                if (*i + *y + *z == 2020) 
-                { 
+    for (auto i = input.begin(); i < input.end() - 2; ++i)
+        for (auto y = i + 1; y < input.end() - 1; ++y)
+        {
+            if (*i + *y < 2020) for (auto z = y + 1; z < input.end(); ++z)
+                if (*i + *y + *z == 2020)
+                {
                     std::cout << *i << " x " << *y << " x " << *z << " = " << (*i) * (*y) * (*z) << std::endl;
-                    found = 1;
+                    goto end;
                 }
+        }
+
+    end:
     return 0;
 }
